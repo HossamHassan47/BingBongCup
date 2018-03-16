4e1802204f20bac2b1f6bee27e29b9b26c4584a0
HossamHassan47 <hossam.hassan@outlook.com>
2018-03-16 08:49:13 +0200

    cup details - select cup player fix

--- app/src/main/java/com/wordpress/hossamhassan47/bingbongcup/adapters/CupPlayerAdapter.java
+++ app/src/main/java/com/wordpress/hossamhassan47/bingbongcup/adapters/CupPlayerAdapter.java
@@ -43,15 +43,15 @@ public class CupPlayerAdapter extends ArrayAdapter<CupPlayerDetails> {
         // Cup Player Number
         TextView txtPlayerNo = listItemView.findViewById(R.id.text_view_cup_player_no);
         txtPlayerNo.setText("#" + (position + 1));
+        TextView txtFullName = listItemView.findViewById(R.id.txtFullName);
+        TextView txtEmail = listItemView.findViewById(R.id.txtEmail);
 
         if (currentItem.player != null) {
-            // Player Name
-            TextView txtFullName = listItemView.findViewById(R.id.txtFullName);
             txtFullName.setText(currentItem.player.fullName);
-
-            // Player Email
-            TextView txtEmail = listItemView.findViewById(R.id.txtEmail);
             txtEmail.setText(currentItem.player.email);
+        } else {
+            txtFullName.setText("");
+            txtEmail.setText("");
         }
 
         return listItemView;
