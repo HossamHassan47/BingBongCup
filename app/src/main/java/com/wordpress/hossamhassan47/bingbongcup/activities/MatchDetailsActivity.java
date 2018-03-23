5bc0766824a348ad9aaf877a1c249842da4cc744
HossamHassan47 <hossam.hassan@outlook.com>
2018-03-23 10:01:18 +0200

    game details

--- app/src/main/java/com/wordpress/hossamhassan47/bingbongcup/activities/MatchDetailsActivity.java
+++ app/src/main/java/com/wordpress/hossamhassan47/bingbongcup/activities/MatchDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.wordpress.hossamhassan47.bingbongcup.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +18,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.wordpress.hossamhassan47.bingbongcup.R;
 import com.wordpress.hossamhassan47.bingbongcup.dao.AppDatabase;
@@ -99,14 +103,101 @@ public class MatchDetailsActivity extends AppCompatActivity {
             return fragment;
         }
 
+        private int gameId = 0;
+        private int player1Id = 0;
+        private int player2Id = 0;
+        private int player1WinningGameCount = 5;
+        private int player2WinningGameCount = 3;
+        private int maximumScore = 21;
+        private int player1Score = 0;
+        private int player2Score = 0;
+
+        TextView txtPlayer1Score;
+        TextView txtPlayer2Score;
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
             View rootView = inflater.inflate(R.layout.fragment_match_details, container, false);
 
-            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            // Winning Game Count
+            LinearLayout linearLayoutPlayer1WinningGameCount = rootView.findViewById(R.id.linear_layout_player_1_winning_game_count);
+            for (int i = 0; i < player1WinningGameCount; i++) {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setImageResource(R.drawable.ic_star_border_white_24dp);
+
+                linearLayoutPlayer1WinningGameCount.addView(imageView);
+            }
+
+            LinearLayout linearLayoutPlayer2WinningGameCount = rootView.findViewById(R.id.linear_layout_player_2_winning_game_count);
+            for (int i = 0; i < player2WinningGameCount; i++) {
+                ImageView imageView = new ImageView(getContext());
+                imageView.setImageResource(R.drawable.ic_star_border_white_24dp);
+
+                linearLayoutPlayer2WinningGameCount.addView(imageView);
+            }
+
+            // Score
+            txtPlayer1Score = rootView.findViewById(R.id.text_view_player_1_score);
+            txtPlayer1Score.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (player1Score < maximumScore) {
+                        player1Score += 1;
+                    }
+
+                    txtPlayer1Score.setText(player1Score + "");
+                }
+            });
+
+            txtPlayer2Score = rootView.findViewById(R.id.text_view_player_2_score);
+            txtPlayer2Score.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (player2Score < maximumScore) {
+                        player2Score += 1;
+                    }
+
+                    txtPlayer2Score.setText(player2Score + "");
+                }
+            });
+
+            // Negative
+            LinearLayout player1Neg = rootView.findViewById(R.id.linear_layout_player_1_neg);
+            player1Neg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (player1Score > 0) {
+                        player1Score -= 1;
+                    }
+
+                    txtPlayer1Score.setText(player1Score + "");
+                }
+            });
+
+            LinearLayout player2Neg = rootView.findViewById(R.id.linear_layout_player_2_neg);
+            player2Neg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (player2Score > 0) {
+                        player2Score -= 1;
+                    }
+
+                    txtPlayer2Score.setText(player2Score + "");
+                }
+            });
+
+            // Save
+            LinearLayout save = rootView.findViewById(R.id.linear_layout_save_match_result);
+            save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(), "Player1: " + player1Score + " Player2: " + player2Score, Toast.LENGTH_SHORT)
+                            .show();
+                }
+            });
+
             return rootView;
         }
     }
